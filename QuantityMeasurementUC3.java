13705b1bb760e9bc6b925107f9fb60c6b90c83ab
divyadharshini165 <vijayalakshmi3181@gmail.com>
2026-04-27 08:44:44 +0530

    UC3: Generic Quantity class with DRY principle

---
+++ QuantityMeasurementUC3.java
@@ -0,0 +1,58 @@
+public class QuantityMeasurementUC3 {
+
+    // Enum for units
+    enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0);
+
+        private final double toFeet;
+
+        LengthUnit(double toFeet) {
+            this.toFeet = toFeet;
+        }
+
+        public double toBase(double value) {
+            return value * toFeet;
+        }
+    }
+
+    // Generic Quantity class
+    static class Quantity {
+        private final double value;
+        private final LengthUnit unit;
+
+        public Quantity(double value, LengthUnit unit) {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Quantity other = (Quantity) obj;
+
+            // Convert both to feet
+            double thisInFeet = this.unit.toBase(this.value);
+            double otherInFeet = other.unit.toBase(other.value);
+
+            return Double.compare(thisInFeet, otherInFeet) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Quantity q1 = new Quantity(1.0, LengthUnit.FEET);
+        Quantity q2 = new Quantity(12.0, LengthUnit.INCH);
+
+        System.out.println("1 ft == 12 inch ? " + q1.equals(q2));
+
+        Quantity q3 = new Quantity(1.0, LengthUnit.INCH);
+        Quantity q4 = new Quantity(1.0, LengthUnit.INCH);
+
+        System.out.println("1 inch == 1 inch ? " + q3.equals(q4));
+    }
+}
